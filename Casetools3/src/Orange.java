df4999f90b6c54b94ff13f09748e54fad195e82d
user <user@DESKTOP-FG4DA2N>
2023-02-23 00:53:17 +0530

    commit first

---
+++ Casetools3/src/Orange.java
@@ -0,0 +1,18 @@
+
+public class Orange {
+	String color;
+	Orange(String color)
+	{
+	this.color=color;
+	}
+
+	public static void main(String[] args) {
+		Orange greenApple = new Orange("green");
+		String color = greenApple.getColor();
+		System.out.println("color of apple is" + color);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
